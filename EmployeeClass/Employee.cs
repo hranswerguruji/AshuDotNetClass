86a7c968046817e7d60e7bc28dcb20ea9d5217ab
santosh90360 <santosh3033@gmail.com>
2026-01-31 09:53:26 +0530

    Crud operation in MVC

---
+++ EmployeeClass/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeClass;
+
+public class Employee
+{
+    public void GetEmployee(string name, int empId, string email)
+    {
+        Console.WriteLine($"Hello {name} your emmployee Id {empId} and email is {email}");
+        Console.WriteLine("Thanks for comming...");
+    }
+}
---
+++ EmployeeClass/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeClass;
+
+Console.WriteLine("Welcome to Ashu program...");
+
+Employee employee = new Employee();
+//employee.GetEmployee("Ashu", 10001, "ashu@gmail.com");
+
+Console.WriteLine("Please provide your details:");
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter your emp id:");
+int empId =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your email address:");
+string email = Console.ReadLine();
+
+employee.GetEmployee(name, empId,email);
+
